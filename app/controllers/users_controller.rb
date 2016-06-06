a16a10091572c58fd0e5d11997ccac5f091b5ca9
Kremenchuk <kremenchuk@bk.ru>
2016-06-06 20:34:22 +0300

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
-    #@users = User.all
-    #redirect_to action: "index"
-  end
-
-  def new
-
-  end
-
   def create
     @user = User.new(params[:user])
 
@@ -28,35 +19,15 @@ class UsersController < ApplicationController
       end
     end
 
-   #@user = User.create(params[:user])
-   #if @user.errors.empty?
-   #  redirect_to action: "index"
-   #  #respond_to do |format|
-   #  #  format.html { redirect_to "users/index"}
-   #  #  format.js
-   #  #end
-   #else
-   #   render json: {success: false}
-   #end
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    #redirect_to action: "index"
+
     render json: {success: true}
   end
 
 
 
 end
-
-
-
-#.col-md-8{:id => "new_user"}
-#.col-md-1 id
-#.col-md-2 First name
-#.col-md-2 Last name
-#.col-md-2 E-mail
-#.col-md-3 Address
-#.col-md-1 Delete user
